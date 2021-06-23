d1cc8ddb354488aa71b7aa001e759e207270ea8d
Doria <dorialassalle@yahoo.com>
2021-06-23 17:31:18 -0400

    Segunda parte validaciones requeridas lista

--- shows_app/models.py
+++ shows_app/models.py
@@ -1,11 +1,36 @@
+from __future__ import unicode_literals
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+#creo mi propio validador en base la los campos de mi tabla que voy a validar, extiende de models.Manager
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors={}   #dicc vacio par guaradr los errores
+
+        if len(postData['title']) < 4:
+            errors['title']="Title should be at least 4 characters" #guardo ene le dicc
+
+        if postData['description'] != "":      #si trae algo, deber ser mayor a 10 caracteres
+            if len(postData['description']) < 10:
+                errors['description']="Description should be at least 10 characters"
+
+        now=datetime.now() #obtengo la fecha actual        
+        release=datetime.strptime(postData['release'], '%Y-%m-%d') #recibo la fecha ingresada que viene como str y la paso a formato de fecha        
+        if release > now:   #comparo ambas fechas que la ingresada no sea mayor a la actual
+            errors['release']="Release Date must be BEFORE today."
+
+        existentTitles=Show.objects.all()   #traigo todos los show de la base y la recorro
+        for showTitle in existentTitles:
+            if postData['title']==showTitle.title:   #comparo el titulo ingresado con los existentes
+                errors['tileDuplicate']="This Show title already exists. Enter another"    
+
+        return errors
 
 class Network(models.Model):
     name=models.CharField(max_length=255) 
     created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    updated_at=models.DateTimeField(auto_now=True)    
     #   shows
 
     def __repr__(self):
@@ -18,13 +43,17 @@ class Show(models.Model):
     title= models.CharField(max_length=255)
     network=models.ForeignKey(Network, related_name="shows", on_delete=models.CASCADE)
     release_date=models.DateField(auto_now=False)
-    description=models.TextField()
+    description=models.TextField(null=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    objects=ShowManager() #vinculo mi validador a la clase
 
     def __repr__(self):
         return f"Show name: {self.title}"
 
     def __str__(self):
         return f"Show: {self.title}"
+
+
+
 
--- shows_app/views.py
+++ shows_app/views.py
@@ -21,15 +21,23 @@ def addShowTemp(request):
     return render(request, "newshow.html", context) # solo nuestra el template para ingresar un nuevo show
 
 def addShow(request):    
-    title=request.POST["title"]
-    network=request.POST["network"]
-    release=request.POST["release"]
-    descr=request.POST["description"] #recibo los datos del formulario
-    objetoNetwork=Network.objects.get(id=network) #traigo el objeto network con el id que lega en el post
-    Show.objects.create(title=title, network=objetoNetwork, release_date=release, description=descr)     
-    newShowAdded=Show.objects.last() #recupero el ultimo objeto creado   
-    newShowId=newShowAdded.id  #obtengo su id
-    return redirect(f"create/{newShowId}") # redirijo con el id
+    errorsFromMyValidator=Show.objects.basic_validator(request.POST)#paso lo que viene en post a mi metodo y lo guardo en la variable   
+    if len(errorsFromMyValidator)>0:  #compruebo si el dicc de errores tiene algo        
+        for key, value in errorsFromMyValidator.items(): #si hay, recorro el dicc y creo el msg flash            
+            messages.error(request, value)
+        
+        return redirect("/shows/new")
+    #si esta vacio agrego a la base:
+    else:
+        title=request.POST["title"]
+        network=request.POST["network"]
+        release=request.POST["release"]
+        descr=request.POST["description"] #recibo los datos del formulario
+        objetoNetwork=Network.objects.get(id=network) #traigo el objeto network con el id que lega en el post
+        Show.objects.create(title=title, network=objetoNetwork, release_date=release, description=descr)     
+        newShowAdded=Show.objects.last() #recupero el ultimo objeto creado   
+        newShowId=newShowAdded.id  #obtengo su id
+        return redirect(f"create/{newShowId}") # redirijo con el id
 
 
 def showCreated(request, id):
@@ -61,17 +69,26 @@ def editShow(request, id):
 
 
 def update(request, id):
-    showToUpdate=Show.objects.get(id=id)#traigo el show por su id y lo guardo
-    showToUpdate.title=request.POST["title"]#guardo lo que viene del post en el campo correspondiente
-    netwId=request.POST["network"]#capturo el id de la network
-    objectNetwork=Network.objects.get(id=netwId)#con el id traigo el objeto
-    showToUpdate.network=objectNetwork #asigno el objeto al campo (de la relacion)
-    showToUpdate.release=request.POST["release"]
-    showToUpdate.description=request.POST["description"]
-    showToUpdate.save() #guardo y actualizo
-    messages.success(request, "The Show has been Updated")
+    errorsFromMyValidator=Show.objects.basic_validator(request.POST)#paso lo que viene en post a mi metodo y lo guardo en la variable   
+    if len(errorsFromMyValidator)>0:  #compruebo si el dicc de errores tiene algo        
+        for key, value in errorsFromMyValidator.items(): #si hay, recorro el dicc y creo el msg flash            
+            messages.error(request, value)
+        
+        return redirect(f"/shows/{id}/edit")
+    #si esta vacio actualizo a la base:
+    else:
 
-    return redirect(f"/shows/{id}/edit") #redirecciono a la pagina edit
+        showToUpdate=Show.objects.get(id=id)#traigo el show por su id y lo guardo
+        showToUpdate.title=request.POST["title"]#guardo lo que viene del post en el campo correspondiente
+        netwId=request.POST["network"]#capturo el id de la network
+        objectNetwork=Network.objects.get(id=netwId)#con el id traigo el objeto
+        showToUpdate.network=objectNetwork #asigno el objeto al campo (de la relacion)
+        showToUpdate.release=request.POST["release"]
+        showToUpdate.description=request.POST["description"]
+        showToUpdate.save() #guardo y actualizo
+        messages.success(request, "The Show has been Updated")
+
+        return redirect(f"/shows/{id}/edit") #redirecciono a la pagina edit
 
 
 def destroy(request, id):
